7d4cd1da5fbd6fedc283b68afed22feb9f17f0cb
Jodi Visser <visser.jodi@gmail.com>
2017-10-13 21:33:19 -0400

    Added Game Report Controller

---
+++ src/ConestogaVirtualGameStore.Web/Controllers/GameReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConestogaVirtualGameStore.Web.Data;
+using ConestogaVirtualGameStore.Web.Models;
+
+namespace ConestogaVirtualGameStore.Web.Controllers
+{
+    public class GameReportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GameReportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GameReport
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Games.ToListAsync());
+        }
+    }
+}
