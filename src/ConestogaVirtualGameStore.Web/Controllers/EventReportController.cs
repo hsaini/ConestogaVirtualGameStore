8d54cfb18ce5a6c581f45dca72db26f1beea0d39
Jodi Visser <visser.jodi@gmail.com>
2017-10-13 22:34:51 -0400

    Final Project Cleanup

---
+++ src/ConestogaVirtualGameStore.Web/Controllers/EventReportController.cs
@@ -0,0 +1,25 @@
+﻿namespace ConestogaVirtualGameStore.Web.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Data;
+    using Models;
+
+    public class EventReportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventReportController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: EventReport
+        public async Task<IActionResult> Index()
+        {
+            return View(await this._context.Events.ToListAsync());
+        }
+    }
+}
